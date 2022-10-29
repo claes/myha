b6fb2e037d3a2aefda96fee26f25f5a96a383675
claes <claes@holmerson.net>
2022-10-29 11:21:30 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "python";
+        venvDir = "./venv";
+        packages = with pkgs; [
+          python39
+          python39Packages.venvShellHook
+        ];
+        postVenvCreation = ''
+          pip install --upgrade pip
+          pip install -r requirements.txt
+          source ./venv/bin/activate
+        '';
+      };
+
+    });
+}
